b675b3714b127a89e376fa929530586795a72a16
Jens Klingen <jens.klingen@consol.de>
2017-10-28 15:07:05 +0200

    Removed hardcoded path to JDK

--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-JAVA=/Library/Java/JavaVirtualMachines/jdk1.9.0_ea+181.jdk/Contents/Home/bin/java
-$JAVA --module-path devday-service/target/devday-service-1.0-SNAPSHOT.jar:devday/target/devday-1.0-SNAPSHOT.jar:devday-workshop-service/target/devday-workshop-service-1.0-SNAPSHOT.jar:devday-talk-service/target/devday-talk-service-1.0-SNAPSHOT.jar -m de.consol.devday/de.consol.devday.Devday
+$JDK_HOME/bin/java --module-path devday-service/target/devday-service-1.0-SNAPSHOT.jar:devday/target/devday-1.0-SNAPSHOT.jar:devday-workshop-service/target/devday-workshop-service-1.0-SNAPSHOT.jar:devday-talk-service/target/devday-talk-service-1.0-SNAPSHOT.jar -m de.consol.devday/de.consol.devday.Devday
